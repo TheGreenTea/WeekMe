f634161a9879c9ff36745671d70d1f587106a24d
David Schmöcker <david.schmoecker@gmail.com>
2018-11-01 21:13:08 +0100

    Added get open tasks call

--- scripts/calls.js
+++ scripts/calls.js
@@ -19,9 +19,14 @@ const login = async () => {
     };
 
     const data = await fetch(loginUrl, settings)
-         .then(response => response.json())
+         .then(response => {
+           console.log(response)
+           console.log(response.headers)
+
+           return response.json()
+         })
          .then(json => {
-              console.log(json);
+             console.log(json);
              return json;
          })
          .catch(e => {
@@ -31,6 +36,38 @@ const login = async () => {
      return data;
 }
 
+// POST /opentasks
+
+const opentasksUrl = baseUrl + '/opentasks';
+const xAuthToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YmRiNTM1Zjg3YzNkZDAwMTU0NjQ1OTUiLCJhY2Nlc3MiOiJhdXRoIiwiaWF0IjoxNTQxMTAwNDAxfQ.F4IWZjHmsIF81apxhqYezfKYumexkwDlSgMzAapYLOg";
+
+
+const getOpenTasks = async () => {
+  const settings = {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'x-auth': xAuthToken
+        }
+    };
+
+    const data = await fetch(opentasksUrl, settings)
+         .then(response => {
+           console.log(response)
+           return response.json()
+         })
+         .then(json => {
+             console.log(json);
+             return json;
+         })
+         .catch(e => {
+             return e
+         });
+
+     return data;
+}
 
 // Execution
 login();
+getOpenTasks();
