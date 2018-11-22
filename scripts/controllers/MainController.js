d327fbfd6c803631392610d7f53c46ca5d789827
Jakub <jakub94@gmx.de>
2018-11-22 19:11:59 +0100

    moving around cards

--- scripts/controllers/MainController.js
+++ scripts/controllers/MainController.js
@@ -12,6 +12,7 @@ var MainController = function() {
 
   function init(){
     setupContainers();
+    initEvents();
 
   }
 
@@ -21,26 +22,32 @@ var MainController = function() {
     setupLabels();
   }
 
+  function initEvents() {
+    $(".card").click(function() {
+
+      if($("#card-selected").length){
+          moveCard($("#card-selected"), this);
+          $("#card-selected").attr("id", "");
+      } else {
+        $(this).attr("id", "card-selected");
+      }
+    });
+  }
+
+  function moveCard(cardToMove, cardToMoveTo){
+    $(cardToMove).parent().insertBefore($(cardToMoveTo).parent());
+  }
+
   function setupLabels(){
-
-
     $("#content-main .row").each(function (index){
-
-
-      const dayLabel = DateFormatter.createDayLabel(index);
+      const dayLabel = DateFormatter.createDayLabel(index); 
       const dateLabel = DateFormatter.createDateLabel(index);
-
-
       let labelContainer = document.createElement('div');
       labelContainer.setAttribute('class', 'label-container');
       labelContainer.appendChild(dayLabel);
       labelContainer.appendChild(dateLabel);
-
       $( labelContainer ).insertBefore($(this));
-
-
     });
-
   }
 
   /* Public Interface */
