6835064a090bad065fb5f60ee6c53576677a45f9
Jakub <jakub94@gmx.de>
2018-11-22 20:04:35 +0100

    move to other day

--- scripts/controllers/MainController.js
+++ scripts/controllers/MainController.js
@@ -23,24 +23,40 @@ var MainController = function() {
   }
 
   function initEvents() {
-    $(".card").click(function() {
+    $(".card").click(function(e) {
+
+      e.stopPropagation();
 
       if($("#card-selected").length){
-          moveCard($("#card-selected"), this);
+          moveCardToPositionOfOtherCard($("#card-selected"), this); 
           $("#card-selected").attr("id", "");
       } else {
         $(this).attr("id", "card-selected");
       }
+
     });
+
+    $(".day-row").click(function(e) {
+
+      if($("#card-selected").length){
+        moveCardToRow($("#card-selected"), $(this));
+        $("#card-selected").attr("id", "");
+      }
+
+    });
+
   }
 
-  function moveCard(cardToMove, cardToMoveTo){
-    $(cardToMove).parent().insertBefore($(cardToMoveTo).parent());
+  function moveCardToPositionOfOtherCard(card, otherCard){
+    $(card).parent().insertBefore($(otherCard).parent());
+  }
+  function moveCardToRow(card, row){
+    $(row).append($(card).parent());
   }
 
   function setupLabels(){
     $("#content-main .row").each(function (index){
-      const dayLabel = DateFormatter.createDayLabel(index); 
+      const dayLabel = DateFormatter.createDayLabel(index);
       const dateLabel = DateFormatter.createDateLabel(index);
       let labelContainer = document.createElement('div');
       labelContainer.setAttribute('class', 'label-container');
