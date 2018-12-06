dbcb24cf8289e4e30d83d568705a2400c590f6d3
JonasHeinig <Jonas-Zapp@web.de>
2018-12-06 20:07:57 +0100

    Color picker implementiert! Cooooooliooo ♥

--- scripts/components/ui_picker.js
+++ scripts/components/ui_picker.js
@@ -6,7 +6,6 @@ var PickerGenerator = function() {
 
   /* Configuration */
 
-
   /* Public Methods */
 
   function init(){
@@ -18,10 +17,12 @@ var PickerGenerator = function() {
     let selectables = "";
 
     for(let i = 0; i < options.length; i++){
-      if(i === selected){
-          selectables += `<option value="${i}" selected="selected">${options[i]}</option>`;
+      if(i === 0){
+          selectables += `<option value="stack">${options[i]}</option>`;
+      } else if(i === selected){
+          selectables += `<option value="${i-1}" selected="selected">${options[i]}</option>`;
       } else {
-          selectables += `<option value="${i}">${options[i]}</option>`;
+          selectables += `<option value="${i-1}">${options[i]}</option>`;
       }
     }
 
@@ -42,7 +43,7 @@ var PickerGenerator = function() {
     let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let options = ["Stack"];
     // If Today: 4 (Thursday)
-    // --> Stack / Today / 5 / 6 / 0 / 1 / 2 / 3 /
+    // --> options: Stack / Today / 5 / 6 / 0 / 1 / 2 / 3 /
     for(let i = day, counter = 0; counter < 7; i=(i+1)%7, counter++){
       if(i === day){
         options.push("Today");
@@ -53,12 +54,11 @@ var PickerGenerator = function() {
     return options;
   }
 
-
   function insertPicker($element){
 
     let options = createOptions();
-
     let newPicker = PickerGenerator.createPicker(2, options);
+
     if($element.children().length === 0){
       $element.append(newPicker);
     }
@@ -66,15 +66,17 @@ var PickerGenerator = function() {
 
   function createNewTask(){
     $("#newTaskStepOne").modal("show");
-    $('#btnDay').hide();
     $('#btnToday').show();
+    $('#btnStack').show();
+    $('#btnDone').hide();
     $('#modalHeadlineStepOne').html("Create new task");
   }
 
   function createNewTaskAtDay(day){
     $("#newTaskStepOne").modal("show");
     $('#btnToday').hide();
-    $('#btnDay').show();
+    $('#btnStack').hide();
+    $('#btnDone').show();
     $('#modalHeadlineStepOne').html("Create new task");
   }
 
@@ -83,31 +85,44 @@ var PickerGenerator = function() {
     $('.new-task-textarea').val(description);
     $('#modalHeadlineStepOne').html("Edit existing task");
     $('#btnToday').hide();
-    $('#btnDay').show();
-  }
-
-  function calculatePickedDay(){
-
-    var day = new Date().getDay();
-    let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    // null = stack oder newTask
-    // 0 = heute (Montag)
-    // 4 = Freitag
-    let pickedDay = (day+4)%7;
-    return pickedDay;
+    $('#btnStack').hide();
+    $('#btnDone').show();
   }
 
   function confirmTask($element, description, day, color){
-    let pickedDay = calculatePickedDay();
-    alert(pickedDay);
+    console.log("Text: " + description + " - At day: " + day);
+  }
+
+  function resetDayPicker(){
+    $('.picker-container').remove();
+    insertPicker($("#pickerPopup"));
+  }
+
+  function closeColorPicker(taskDescription){
+    $("#newTaskColorPicker").modal("hide");
+    $("#newTaskStepOne").modal("show");
+    $('.new-task-textarea').val(taskDescription);
+  }
+
+  function checkIfColorShouldBeResetted(isStepOneShown, isStepTwoShown, isColorPickerShown){
+    if(!isStepOneShown && !isStepTwoShown && !isColorPickerShown){
+      var modals = $(".modal-content");
+      $(".modal-content").removeClass();
+      modals.addClass('modal-content');
+    }
   }
 
   $(document).ready(() => {
 
     var taskDescription = "";
+    var taskColor = "";
+    var isStepOneShown = false;
+    var isStepTwoShown = false;
+    var isColorPickerShown = false;
+
     insertPicker($("#pickerPopup"));
 
+// Button zum öffnen des Popup >>
     $("#btnNewTask").click(() => {
       createNewTask();
     });
@@ -118,15 +133,43 @@ var PickerGenerator = function() {
     });
 
     $("#btnEditTaskDay").click(() => {
-      let description = "Schon existierender Task, der editiert werden kann";
       let $element = "";
+      let text = "Schon existierender Task, der editiert werden kann";
       let day = 4;
       let color = 5;
-      editTask(description, $element, day, color);
+      editTask($element, text, day, color);
     });
 
-    $("#btnChangeColor").click(() => {
-      alert("Color picker");
+
+// Step 1
+    $("#newTaskStepOne").on('show.bs.modal', function () {
+      isStepOneShown = true;
+    });
+
+    $("#newTaskStepOne").on('hidden.bs.modal', function () {
+      taskDescription = $('.new-task-textarea').val();
+      $('.new-task-textarea').val("");
+      isStepOneShown = false;
+      checkIfColorShouldBeResetted(isStepOneShown, isStepTwoShown, isColorPickerShown);
+    });
+
+    $("#btnToday").click(() => {
+      $("#newTaskStepOne").modal("hide");
+      var description = $('.new-task-textarea').val();
+      confirmTask("", description, 0,"");
+    });
+
+    $("#btnStack").click(() => {
+      $("#newTaskStepOne").modal("hide");
+      var description = $('.new-task-textarea').val();
+      confirmTask("", description, "Stack","");
+    });
+
+    $("#btnDone").click(() => {
+      $("#newTaskStepOne").modal("hide");
+      var description = $('.new-task-textarea').val();
+      let day = 1; // hier muss der vom Klick übergebene Tag eingefügt werden (jenachdem in welchem Tag der Task angeklickt wurde)
+      confirmTask("", description, day,"");
     });
 
     $("#btnPickDay").click(() => {
@@ -134,21 +177,99 @@ var PickerGenerator = function() {
       $("#newTaskStepTwo").modal("show");
     });
 
+
+// Step 2
     $("#btnBackToEditTask").click(() => {
       $("#newTaskStepTwo").modal("hide");
       $("#newTaskStepOne").modal("show");
       $('.new-task-textarea').val(taskDescription);
     });
 
-    $("#newTaskStepOne").on('hidden.bs.modal', function () {
-      taskDescription = $('.new-task-textarea').val();
-      $('.new-task-textarea').val("");
-    });
-
     $("#btnConfirmTask").click(() => {
       $("#newTaskStepTwo").modal("hide");
-      confirmTask("","","","");
+      let day = $("select option:selected").val();
+      confirmTask("", taskDescription, day,"");
     });
+
+    $("#newTaskStepTwo").on('show.bs.modal', function () {
+      isStepTwoShown = true;
+    });
+
+    $("#newTaskStepTwo").on('hidden.bs.modal', function () {
+      resetDayPicker();
+      isStepTwoShown = false;
+      checkIfColorShouldBeResetted(isStepOneShown, isStepTwoShown, isColorPickerShown);
+    });
+
+
+// Color Picker >>
+    $("#btnChangeColor").click(() => {
+      $("#newTaskStepOne").modal("hide");
+      $("#newTaskColorPicker").modal("show");
+      taskDescription = $('.new-task-textarea').val();
+    });
+
+    $("#btnBackColorPicker").click(() => {
+      closeColorPicker(taskDescription);
+    });
+
+    $("#btnColorWhite").click(() => {
+      closeColorPicker(taskDescription);
+      taskColor = 0;
+      var modals = $(".modal-content");
+      $(".modal-content").removeClass();
+      modals.addClass('modal-content white');
+    });
+
+    $("#btnColorYellow").click(() => {
+      closeColorPicker(taskDescription);
+      taskColor = 1;
+      var modals = $(".modal-content");
+      $(".modal-content").removeClass();
+      modals.addClass('modal-content yellow');
+    });
+
+    $("#btnColorRed").click(() => {
+      closeColorPicker(taskDescription);
+      taskColor = 2;
+      var modals = $(".modal-content");
+      $(".modal-content").removeClass();
+      modals.addClass('modal-content red');
+    });
+
+    $("#btnColorPurple").click(() => {
+      closeColorPicker(taskDescription);
+      taskColor = 3;
+      var modals = $(".modal-content");
+      $(".modal-content").removeClass();
+      modals.addClass('modal-content purple');
+    });
+
+    $("#btnColorBlue").click(() => {
+      closeColorPicker(taskDescription);
+      taskColor = 4;
+      var modals = $(".modal-content");
+      $(".modal-content").removeClass();
+      modals.addClass('modal-content blue');
+    });
+
+    $("#btnColorGreen").click(() => {
+      closeColorPicker(taskDescription);
+      taskColor = 5;
+      var modals = $(".modal-content");
+      $(".modal-content").removeClass();
+      modals.addClass('modal-content green');
+    });
+
+    $("#newTaskColorPicker").on('show.bs.modal', function () {
+      isColorPickerShown = true;
+    });
+
+    $("#newTaskColorPicker").on('hidden.bs.modal', function () {
+      isColorPickerShown = false;
+      checkIfColorShouldBeResetted(isStepOneShown, isStepTwoShown, isColorPickerShown);
+    });
+
   });
 
   /* Public Interface */
